a8d7de713e2361e1afe79495e0d4b841dfb1358d
maxly20 <maxly2011@gmail.com>
2021-04-13 13:33:32 +1000

    finish search

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ let dataChart = [];
 
 const API_URL = 'https://api.covid19api.com/summary';
 
-const covid = async () => {
+const covid = async country => {
   const res = await fetch(API_URL);
   const data = await res.json();
   console.log(data);
@@ -19,28 +19,56 @@ const covid = async () => {
   if (res.status === 4 || res.status === 200) {
     date.textContent = new Date(data.Date).toDateString();
 
-    const {
-      TotalConfirmed,
-      TotalDeaths,
-      TotalRecovered,
-      NewConfirmed,
-      NewDeaths,
-      NewRecovered,
-    } = data.Global;
-// TOTAL NEW CASE
-    confirmed.children[1].textContent = TotalConfirmed;
-    confirmed.children[2].textContent = NewConfirmed;
-// TOTAL DEATHS
-    confirmed.children[1].textContent = TotalConfirmed;
-    confirmed.children[2].textContent = NewConfirmed;
-// TOTAL RECOVERED
-    confirmed.children[1].textContent = TotalConfirmed;
-    confirmed.children[2].textContent = NewConfirmed;
+    if (country === '' || country === 'World') {
+      const {
+        TotalConfirmed,
+        TotalDeaths,
+        TotalRecovered,
+        NewConfirmed,
+        NewDeaths,
+        NewRecovered,
+      } = data.Global;
+      // TOTAL NEW CASE
+      confirmed.children[1].textContent = TotalConfirmed;
+      confirmed.children[2].textContent = NewConfirmed;
+      // TOTAL DEATHS
+      deaths.children[1].textContent = TotalDeaths;
+      deaths.children[2].textContent = NewDeaths;
+      // TOTAL RECOVERED
+      recovered.children[1].textContent = TotalRecovered;
+      recovered.children[2].textContent = NewRecovered;
 
+      nameCountry.textContent = 'The World';
+    }
 
+    data.Countries.forEach(item => {
+      const option = document.createElement('option');
+      option.value = item.Country;
+      option.textContent = item.Country;
+      countries.appendChild(option);
+      if (country === item.Country) {
+        // TOTAL NEW CASE
+        confirmed.children[1].textContent = item.TotalConfirmed;
+        confirmed.children[2].textContent = item.NewConfirmed;
+        // TOTAL DEATHS
+        deaths.children[1].textContent = item.TotalDeaths;
+        deaths.children[2].textContent = item.NewDeaths;
+        // TOTAL RECOVERED
+        recovered.children[1].textContent = item.TotalRecovered;
+        recovered.children[2].textContent = item.NewRecovered;
 
+        nameCountry.textContent = item.Country;
+      }
+    });
   } else {
     chart.innerHTML = `<h2>Loading......</h2>`;
   }
 };
-covid();
+covid(search.value);
+
+const btnSearch = document.querySelector('button');
+btnSearch.addEventListener('click', e => {
+  e.preventDefault();
+  covid(search.value);
+  search.value = '';
+});
